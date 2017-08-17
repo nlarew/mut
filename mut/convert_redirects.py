9ed1a3a941e0dbd1b29872cf96f8e598ecca5274
Nick Larew <nick.larew@10gen.com>
2017-08-17 14:07:51 -0500

    refactor transform_version_rule()

--- mut/convert_redirects.py
+++ mut/convert_redirects.py
@@ -1,33 +1,42 @@
 """
 Usage:
-    mut-convert-redirects [-o PATH] <path>
+    mut-convert-redirects -b BASE [-o PATH] <files>
 
     -h, --help              List CLI prototype, arguments, and options.
+    -b BASE                 Base URL of the property
     -o PATH                 Output path
     <files>                 Path to the directory containing html files.
 """
 import re
-from typing import List
+from typing import List, NamedTuple
 
 from docopt import docopt
 import yaml
+
+Output = NamedTuple('Output', [('property', str), ('version', str)])
 
 
 def transform_version_rule(rule: str) -> str:
     """Transform a giza-style version rule to a mut-style version rule."""
     if rule == 'all':
         return '[*]'
+    else:
+        match = re.match(r'((?:after)|(?:before)|)-?(.*)', rule)
+        predicate = match.group(1)
+        version = match.group(2)
+        templates = {
+            'before': '[*-{}]',
+            'after': '({}-*]',
+        }
+        template = templates[predicate] if predicate in templates else '[{}]'
+        return template.format(version)
 
-    match = re.match(r'((?:after)|(?:before)|)-?(.*)', rule)
-    if match.group(1) == 'after':
-        return '({}-*]'.format(match.group(2))
-    elif match.group(2) == 'before':
-        return '(*-{}]'.format(match.group(2))
 
-    return '[{}]'.format(match.group(2))
+# def parse_output(output) -> List[Output]:
+#     return []
 
 
-def convert_file(path: str) -> List[str]:
+def convert_file(path: str, base: str) -> List[str]:
     """Convert a giza-style redirect file to a list of mut-style rules."""
     with open(path, 'r') as f:
         redirects = list(yaml.safe_load_all(f))
@@ -38,19 +47,24 @@ def convert_file(path: str) -> List[str]:
             continue
 
         for output in rule['outputs']:
-            if isinstance(output, str):
+            #po = parse_output(output)
+            if isinstance(output, str): # e.g. 'v2.2'
                 version = transform_version_rule(output)
                 base_component = ''
             elif isinstance(output, dict):
                 output = list(output.items())[0]
-
-                if output[0][0] == '/':
+                if isinstance(output[1], dict):  # e.g. 'after-v1.x': {'/ruby-driver[/v2.x]': 'https://docs.mongodb.com/ruby-driver'}
+                    if output[0][0] == '/':
+                        version = 'raw'
+                        base_component = output[0]
+                    else:
+                        version = transform_version_rule(output[0])
+                        base_component = output[1] if isinstance(output[1], str) else list(output[1].keys())[0]
+                elif isinstance(output[1], str):  # Then it is of form: {'': 'https://docs.atlas.mongodb.com'}
                     version = 'raw'
-                    base_component = output[0]
-                else:
-                    version = transform_version_rule(output[0])
-                    base_component = output[1] if isinstance(output[1], str) else list(output[1].keys())[0]
+                    base_component = output[1]
 
+            # Assemble Redirect
             if version is 'raw':
                 rule['from'] = '/'.join((base_component.rstrip('/'), rule['from'].lstrip('/')))
                 rule['to'] = '/'.join((base_component.rstrip('/'), rule['to'].lstrip('/')))
@@ -66,7 +80,7 @@ def convert_file(path: str) -> List[str]:
 def main() -> None:
     options = docopt(__doc__)
 
-    result = '\n'.join(convert_file(options['<path>']))
+    result = '\n'.join(convert_file(options['<files>'], options['-b']))
     if options['-o']:
         with open(options['-o'], 'w') as f:
             f.write(result)
